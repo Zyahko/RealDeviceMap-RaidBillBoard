a120087bf742b3117ca841c6adfaa42e6b27d377
Zyahko <20727981+Zyahko@users.noreply.github.com>
2019-01-24 16:10:43 -0600

    Update raid.php
    Forgot the geofence part  in the sql statement

--- raid.php
+++ raid.php
@@ -18,7 +18,19 @@ try{
 // Query Database and Build Raid Billboard
 try 
 {
-    $sql = "SELECT time_format(from_unixtime(raid_battle_timestamp), '%h:%i:%s %p'), time_format(from_unixtime(raid_end_timestamp),'%h:%i:%s %p'), raid_level, pokedex.name, quick_movedex.name, charge_movedex.name, gym.name, gym.lat, gym.lon from gym join pokedex on gym.raid_pokemon_id = pokedex.pokemon_id join quick_movedex on gym.raid_pokemon_move_1 = quick_movedex.move_id join charge_movedex on gym.raid_pokemon_move_2 = charge_movedex.move_id where raid_pokemon_id is not null && gym.name is not null order by raid_end_timestamp";   
+    $sql = "SELECT time_format(from_unixtime(raid_battle_timestamp), '%h:%i:%s %p'), 
+           time_format(from_unixtime(raid_end_timestamp),'%h:%i:%s %p'), 
+           raid_level, pokedex.name, quick_movedex.name, charge_movedex.name, gym.name, gym.lat, gym.lon from gym 
+           join pokedex on gym.raid_pokemon_id = pokedex.pokemon_id 
+           join quick_movedex on gym.raid_pokemon_move_1 = quick_movedex.move_id 
+           join charge_movedex on gym.raid_pokemon_move_2 = charge_movedex.move_id 
+           where where ST_CONTAINS(ST_GEOMFROMTEXT('POLYGON((
+           LAT -LONG, 
+           LAT -LONG))') 
+           point(gym.lat, gym.lon)) && 
+           raid_pokemon_id is not null && 
+           gym.name is not null 
+           order by raid_end_timestamp";   
         $result = $pdo->query($sql);
         if($result->rowCount() > 0){
             echo "<table border='1';>";
